2cb7640d03e4f61c4a079bae964517fafccd0d6f
SUNWOOPARK <psw261@gmail.com>
2019-11-09 16:49:37 +0900

    init commit

---
+++ shop/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, re_path, include, register_converter
+from .converters import FourDigitYearConverter
+from .import views
+
+register_converter(FourDigitYearConverter, 'yyyy')
+
+app_name = 'shop'
+
+urlpatterns = [
+    path('archives/<yyyy:year>/', views.archives_year),
+]
