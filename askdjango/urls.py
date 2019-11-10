125c2dd3aa673a4a8728724cf138ecbb99cecbed
SUNWOOPARK <psw261@gmail.com>
2019-11-11 05:52:47 +0900

    add blog model and migrations

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, re_path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
+    path('blog/', include('blog.urls')),
 ]
---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path, re_path, include, register_converter
+from .import views
+
+app_name = 'blog'
+
+urlpatterns = [
+]
